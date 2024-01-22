a3bbf1e287b109ef45da335fa661a8af00818c55
Ait Hamid Adam <106628849+aithamid@users.noreply.github.com>
2024-01-22 17:40:28 +0100

    - Traffic Conditions - Physical Infrastructure - Scenery - Environmental conditions finished

--- src/app/data/datasource.tsx
+++ src/app/data/datasource.tsx
@@ -404,10 +404,594 @@ export let data: object[] = [
 },
 {
     'Id': 'l3-25',
-    'Name': 'Number of branches of the intersection branch',
+    'Name': 'Particularity of the intersection',
     'Designation': '',
     'IsExpand': 'false',
     'RatingColor': '#68C2DE',
     'ReportingPerson': 'l2-4'
+},
+{
+    'Id': 'l3-26',
+    'Name': 'Workzone',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-5'
+},
+{
+    'Id': 'l3-27',
+    'Name': 'FLR',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-5'
+},
+{
+    'Id': 'l3-28',
+    'Name': 'Constraints on masks from the visibility point of view',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-6'
+},
+{
+    'Id': 'l3-29',
+    'Name': 'Constraints on masks from the geo-positioning point of view',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-6'
+},
+{
+    'Id': 'l3-30',
+    'Name': 'Crossing of vulnerable users',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-7'
+},
+{
+    'Id': 'l3-31',
+    'Name': 'Marking of cycle zone on the ego lane',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-7'
+},
+{
+    'Id': 'l3-32',
+    'Name': 'Marking of cycle zone on the ego lane',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-7'
+},
+{
+    'Id': 'l3-33',
+    'Name': 'Vertical traffic signs',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-8'
+},
+{
+    'Id': 'l4-13',
+    'Name': 'Type A : hazard',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-33'
+},
+{
+    'Id': 'l4-14',
+    'Name': 'Type AB : priorities at intersection ',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-33'
+},
+{
+    'Id': 'l4-15',
+    'Name': 'Type B prescriptions',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-33'
+},
+{
+    'Id': 'l4-16',
+    'Name': 'Type C useful indications',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-33'
+},
+{
+    'Id': 'l4-17',
+    'Name': 'Type CE useful services indication',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-33'
+},
+{
+    'Id': 'l4-18',
+    'Name': 'Type D signalization and positioning',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-33'
+},
+{
+    'Id': 'l4-19',
+    'Name': 'Type E entries/end of urban zones',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-33'
+},
+{
+    'Id': 'l4-20',
+    'Name': 'Type G Railway crossing localization',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-33'
+},
+{
+    'Id': 'l4-21',
+    'Name': 'Type SR information for road safety',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-33'
+},
+{
+    'Id': 'l3-34',
+    'Name': 'Traffic lights',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-8'
+},
+{
+    'Id': 'l3-35',
+    'Name': 'Road markings (horizontal markings)',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-8'
+},
+{
+    'Id': 'l3-36',
+    'Name': 'Guidance equipment',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-8'
+},
+{
+    'Id': 'l3-37',
+    'Name': 'Boundary markers',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-8'
+},
+{
+    'Id': 'l3-38',
+    'Name': 'Closing devices',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-8'
+},
+{
+    'Id': 'l3-39',
+    'Name': 'Dynamic signs ',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-8'
+},
+{
+    'Id': 'l3-40',
+    'Name': 'Temporary signs',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-8'
+},
+{
+    'Id': 'l4-22',
+    'Name': 'Type AK : hazard',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-40'
+},
+{
+    'Id': 'l4-23',
+    'Name': 'Type K : Specific',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-40'
+},
+{
+    'Id': 'l4-24',
+    'Name': 'Type KC : Workzones',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-40'
+},
+{
+    'Id': 'l4-25',
+    'Name': 'Type KD : Lane merging',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-40'
+},
+{
+    'Id': 'l3-41',
+    'Name': 'Connected equipments',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-8'
+},
+{
+    'Id': 'l3-42',
+    'Name': 'Infrared objects',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-9'
+},
+{
+    'Id': 'l3-43',
+    'Name': 'Beacon',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-9'
+},
+{
+    'Id': 'l3-44',
+    'Name': 'Speed bump',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-9'
+},
+{
+    'Id': 'l3-45',
+    'Name': 'Electromagnetic',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-9'
+},
+{
+    'Id': 'l2-55',
+    'Name': 'Specific zones',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l1-2'
+},
+{
+    'Id': 'l2-56',
+    'Name': 'Region/State',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l1-2'
+},
+{
+    'Id': 'l2-57',
+    'Name': 'Interference zones',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l1-2'
+},
+{
+    'Id': 'l2-58',
+    'Name': 'Geo-fencing',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l1-2'
+},
+{
+    'Id': 'l2-59',
+    'Name': 'Landmark',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l1-2'
+},
+{
+    'Id': 'l2-60',
+    'Name': 'Weather conditions',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l1-3'
+},
+{
+    'Id': 'l3-46',
+    'Name': 'Rain',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-60'
+},
+{
+    'Id': 'l3-47',
+    'Name': 'Snow',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-60'
+},
+{
+    'Id': 'l3-48',
+    'Name': 'Hall',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-60'
+},
+{
+    'Id': 'l3-49',
+    'Name': 'Fog',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-60'
+},
+{
+    'Id': 'l3-49',
+    'Name': 'Wind',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-60'
+},
+{
+    'Id': 'l2-61',
+    'Name': 'Particulates',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l1-3'
+},
+{
+    'Id': 'l2-62',
+    'Name': 'Weather-induced roadway conditions',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l1-3'
+},
+{
+    'Id': 'l2-63',
+    'Name': 'Illumination',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l1-3'
+},
+{
+    'Id': 'l3-50',
+    'Name': 'Illumination level',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-63'
+},
+{
+    'Id': 'l3-51',
+    'Name': 'Illumination conditions',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-63'
+},
+{
+    'Id': 'l3-52',
+    'Name': 'Illumination illuminances',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-63'
+},
+{
+    'Id': 'l3-53',
+    'Name': 'Illumination variations',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-63'
+},
+{
+    'Id': 'l2-64',
+    'Name': 'Minimum/Maximum ambient air temperature',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l1-3'
+},
+{
+    'Id': 'l2-65',
+    'Name': 'Traffic density',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l1-4'
+},
+{
+    'Id': 'l3-54',
+    'Name': 'Traffic density on the ego direction traffic lane(s)',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-65'
+},
+{
+    'Id': 'l3-55',
+    'Name': 'Traffic density on the opposite direction traffic lane(s) ',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-65'
+},
+{
+    'Id': 'l3-56',
+    'Name': 'Traffic density on ramp in',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-65'
+},
+{
+    'Id': 'l3-57',
+    'Name': 'Traffic density on ramp off',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-65'
+},
+{
+    'Id': 'l3-58',
+    'Name': 'Traffic density on the crossing lane(s)',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-65'
+},
+{
+    'Id': 'l3-59',
+    'Name': 'Traffic density on the traffic lane(s) n°i',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-65'
+},
+{
+    'Id': 'l2-66',
+    'Name': 'Road-users (type & speed)',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l1-4'
+},
+{
+    'Id': 'l3-60',
+    'Name': 'Road-users type',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-66'
+},
+{
+    'Id': 'l4-26',
+    'Name': 'Road-users type on the ego direction traffic lane(s)',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-60'
+},
+{
+    'Id': 'l4-27',
+    'Name': 'Road-users type on the opposite direction traffic lane(s)',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-60'
+},
+{
+    'Id': 'l4-28',
+    'Name': 'Road-users type on the crossing traffic lane(s)',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-60'
+},
+{
+    'Id': 'l4-29',
+    'Name': 'Road-users type on the traffic lane n°i',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-60'
+},
+{
+    'Id': 'l3-61',
+    'Name': 'Road-users speed',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-66'
+},
+{
+    'Id': 'l4-30',
+    'Name': 'Road users speed on the ego direction traffic lane(s)',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-61'
+},
+{
+    'Id': 'l4-31',
+    'Name': 'Road users speed on the opposite direction traffic lane(s)',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-61'
+},
+{
+    'Id': 'l4-32',
+    'Name': 'Road users speed on the crossing traffic lane(s)',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-61'
+},
+{
+    'Id': 'l4-33',
+    'Name': 'Road users speed on the traffic lane n° i',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l3-61'
+},
+{
+    'Id': 'l2-67',
+    'Name': 'Road users behavior',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l1-4'
+},
+{
+    'Id': 'l3-62',
+    'Name': 'Risky behavior of third parties',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-67'
+},
+{
+    'Id': 'l3-63',
+    'Name': 'Particularly accident-prone areas',
+    'Designation': '',
+    'IsExpand': 'false',
+    'RatingColor': '#68C2DE',
+    'ReportingPerson': 'l2-67'
 },
 ];
