f4731841d3d4b7eac4e45e326e9bde599715e9ab
Ait Hamid Adam <106628849+aithamid@users.noreply.github.com>
2024-02-27 17:36:19 +0100

    Remove toast

--- src/components/login/loginform.tsx
+++ src/components/login/loginform.tsx
@@ -27,14 +27,7 @@ export default function LoginForm() {
   });
 
   function onSubmit(data: FormData) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    });
+
   }
 
   return (
